69c88ea02babce2b878ad7ed0818bd37aede2a77
guhan <smguhan1926@gmail.com>
2025-02-20 22:28:15 +0530

    Update in Contact File

--- pages/contact.py
+++ pages/contact.py
@@ -5,11 +5,15 @@ st.title("Contact Me")
 
 with st.form(key="email"):
     user_email = st.text_input("Your Mail ID")
+    topic = st.selectbox("what topic do you want to discuss?",
+                         ("Python Basis", "Python Project", "Other Python Queries", "Other Information")
+                         , placeholder="Select The Topic...")
     raw_message = st.text_area("Your Message")
     message = f"""\
 Subject:Email from {user_email}
 
 From: {user_email}
+Topic: {topic}
 {raw_message}
     """
     button = st.form_submit_button("Submit")
